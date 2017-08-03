bcabec144699d56e0d4447c6a0cb8dbee3fbecba
Jessie Wu <zongxinwu92@gmail.com>
2017-08-02 20:22:38 -0700

    change the style to stisfy PEP8

--- entertainment_center.py
+++ entertainment_center.py
@@ -7,15 +7,20 @@ tbbt = media.Movie("The Big Bang Theory",
 
 
 modern_family = media.Movie("Modern Family",
-                            "https://images.tenplay.com.au/~/media/TV%20Shows/Modern%20Family/Season%208/ModernFamily_S8_Logo_620x349.jpg",
+                            ("https://images.tenplay.com.au/~/media/TV%20Shows"
+                             "/Modern%20Family/Season%208/ModernFamily_S8_Logo"
+                             "_620x349.jpg"),
                             "https://www.youtube.com/watch?v=meF4MfQ3jMw")
 
 broken_girls = media.Movie("2 Broken Girls",
-                           "http://i.gtimg.cn/qqlive/img/jpgcache/files/qqvideo/2/2ih1wnad4h30y5n.jpg",
+                           "http://i.gtimg.cn/qqlive/img/jpgcache/files/"
+                           "qqvideo/2/2ih1wnad4h30y5n.jpg",
                            "https://www.youtube.com/watch?v=f-V6dYUkneU")
 
 flipped = media.Movie("Flipped",
-                      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTU2NjQ1Nzc4MF5BMl5BanBnXkFtZTcwNTM0NDk1Mw@@._V1_UY1200_CR89,0,630,1200_AL_.jpg",
+                      ("https://images-na.ssl-images-amazon."
+                       "com/images/M/MV5BMTU2NjQ1Nzc4MF5BMl5BanBnXkFtZ"
+                       "TcwNTM0NDk1Mw@@._V1_UY1200_CR89,0,6301200_AL_.jpg"),
                       "https://www.youtube.com/watch?v=RDlXdujRSD8")
 
 movies = [tbbt, modern_family, broken_girls, flipped]
