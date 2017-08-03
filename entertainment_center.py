0a9a3528c34a8710501ed58d719f82948343e7b2
Jessie Wu <zongxinwu92@gmail.com>
2017-08-02 19:11:09 -0700

    finish media and entertainmen_center

---
+++ entertainment_center.py
@@ -0,0 +1,23 @@
+from project1 import media
+from project1.ud036_StarterCode import fresh_tomatoes
+
+tbbt = media.Movie("The Big Bang Theory",
+                   "https://images8.alphacoders.com/431/431311.jpg",
+                   "https://www.youtube.com/watch?v=PAm8PV47jMM")
+
+
+modern_family = media.Movie("Modern Family",
+                            "https://images.tenplay.com.au/~/media/TV%20Shows/Modern%20Family/Season%208/ModernFamily_S8_Logo_620x349.jpg",
+                            "https://www.youtube.com/watch?v=meF4MfQ3jMw")
+
+broken_girls = media.Movie("2 Broken Girls",
+                           "http://i.gtimg.cn/qqlive/img/jpgcache/files/qqvideo/2/2ih1wnad4h30y5n.jpg",
+                           "https://www.youtube.com/watch?v=f-V6dYUkneU")
+
+flipped = media.Movie("Flipped",
+                      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTU2NjQ1Nzc4MF5BMl5BanBnXkFtZTcwNTM0NDk1Mw@@._V1_UY1200_CR89,0,630,1200_AL_.jpg",
+                      "https://www.youtube.com/watch?v=RDlXdujRSD8")
+
+movies = [tbbt, modern_family, broken_girls, flipped]
+
+fresh_tomatoes.open_movies_page(movies)
---
+++ media.py
@@ -0,0 +1,15 @@
+import webbrowser
+
+"""create a website to show my favorite movies"""
+
+class Movie():
+    def __init__(self, movie_title, poster_image_url, trailer_youtube_id):
+        self.title = movie_title
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_id = trailer_youtube_id
+
+    def open_website(self):
+        # change default website as Chrome
+        chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
+        url = self.trailer_youtube_id
+        webbrowser.get(chrome_path).open(url)
