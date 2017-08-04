58477755bc6b0a36683df15040c49fce723373c3
Jessie Wu <zongxinwu92@gmail.com>
2017-08-04 16:28:23 -0700

    change the html file

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# udacityProject1
+project1 is about showing my favorite movies.
+The codes are based on python 3.5 and contains three parts.
+Firstly, the package called ud036_StarterCode which contains fresh_tomatoes file.
+Secondly,the media which defines class.
+Thirdly, the entertainment_center file which are instances.
+To open the website, only need to run entertainment_center python program.
+
--- entertainment_center.py
+++ entertainment_center.py
@@ -1,15 +1,16 @@
 from project1 import media
 from project1.ud036_StarterCode import fresh_tomatoes
 
+"""the instances and show final website"""
+
 tbbt = media.Movie("The Big Bang Theory",
-                   "https://images8.alphacoders.com/431/431311.jpg",
+                   ("http://i.cdn.turner.com/v5cache/TBS/Images/Dynamic/"
+                    "i450/vizio-the-big-bang-theory-960x1440.png"),
                    "https://www.youtube.com/watch?v=PAm8PV47jMM")
 
-
 modern_family = media.Movie("Modern Family",
-                            ("https://images.tenplay.com.au/~/media/TV%20Shows"
-                             "/Modern%20Family/Season%208/ModernFamily_S8_Logo"
-                             "_620x349.jpg"),
+                            ("https://almostelysian.files.wordpress.com/"
+                             "2015/09/modern-family.jpg?w=640"),
                             "https://www.youtube.com/watch?v=meF4MfQ3jMw")
 
 broken_girls = media.Movie("2 Broken Girls",
@@ -23,6 +24,16 @@ flipped = media.Movie("Flipped",
                        "TcwNTM0NDk1Mw@@._V1_UY1200_CR89,0,6301200_AL_.jpg"),
                       "https://www.youtube.com/watch?v=RDlXdujRSD8")
 
-movies = [tbbt, modern_family, broken_girls, flipped]
+house_of_cards = media.Movie("House of Cards",
+                            ("http://img1.gtimg.com/tech/pics/hv1/18/14/1259/"
+                             "81870063.jpg"),
+                            "https://www.youtube.com/watch?v=8QnMmpfKWvo")
+
+devious_maids = media.Movie("Devious Maids",
+                            ("http://i.gtimg.cn/qqlive/img/jpgcache/files/"
+                             "qqvideo/q/qk8vyb5drwnn174.jpg"),
+                            "https://www.youtube.com/watch?v=JAh-5K2F7dI")
+
+movies = [tbbt, modern_family, broken_girls, flipped, house_of_cards, devious_maids]
 
 fresh_tomatoes.open_movies_page(movies)
